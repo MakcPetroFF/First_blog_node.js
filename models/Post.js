3aa9838b3dbc10a9b5987bd7143e21e50c33cac6
Max.Vinnikov <krasagent@bk.ru>
2019-03-23 22:36:24 +0700

    Создание роутера, подключение МонгоДБ

--- models/Post.js
+++ models/Post.js
@@ -11,7 +11,7 @@ const postSchema = new Schema({
         required: true
     },
     date: {
-        type: SDate,
+        type: Date,
         default: Date.now
     }
 
--- routes/post.js
+++ routes/post.js
@@ -2,14 +2,12 @@ const express = require('express');
 const router = express.Router();
 const Post = require('../models/Post');
 
-router.get('/', async(req, res) => {
-    const posts = await Post.find({});
-    res.status(200).json(posts);
-
-    });
+router.get('/', async (req, res) => {
+    const posts = await Post.find({})
+    res.status(200).json(posts)
 });
 
-router.post('/', async(req, res) => {
+router.post('/', async (req, res) => {
     const postData = {
         title: req.body.title,
         text: req.body.text
@@ -21,15 +19,11 @@ router.post('/', async(req, res) => {
     res.status(201).json(post);
 });
 
-router.delete('/:Id', async(req, res) => {
-    Post.remove({_id: req.params.Id});
+router.delete('/:postId', async (req, res) => {
+    Post.remove({_id: req.params.postId})
     res.status(200).json({
         message: 'Удалено'
     });
 });
-
-
-
-
 
 module.exports = router;
